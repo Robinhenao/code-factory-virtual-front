88345e7d07f239b25cd9d1e366091c8deab16fed
Robinson Henao <robinson.henao@udea.edu.co>
2024-11-15 23:49:55 -0500

    fix logo 2

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -5,10 +5,10 @@ const Logo = () => (
     <div className="flex">
         <Link href="/#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
             <Image
-                src="/logo2.png"
+                src='/logo2.png'
                 alt="Acme Inc Logo"
                 width={120}
-                height={36}
+                height={120}
                 className="h-24 w-24"
                 priority
             />
