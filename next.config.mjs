b726c64c88d545d94994a921257e7145e06e931c
Robinson Henao <robinson.henao@udea.edu.co>
2024-11-16 00:07:02 -0500

    fix logo 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.imgur.com'],  // Permite imágenes desde Imgur
+  },
 };
 
 export default nextConfig;
--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
+// Loader personalizado
+const imageLoader = ({ src, width, quality = 75 }: { src: string, width: number, quality?: number }) => {
+  return `https://i.imgur.com/${src}?w=${width}&q=${quality}`;
+};
+
 const Logo = () => (
-    <div className="flex">
-        <Link href="/#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-            <Image
-                src='/logo2.png'
-                alt="Acme Inc Logo"
-                width={120}
-                height={120}
-                className="h-24 w-24"
-                priority
-            />
-            <span className="font-roboto font-black text-white text-[6vw] sm:text-[5vw] md:text-[3vw]">Singapur</span>
-        </Link>
-    </div>
+  <div className="flex">
+    <Link href="/#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+      <Image
+        loader={imageLoader}  // Usar el loader personalizado
+        src="ITf6Huo.png"  // Solo el nombre del archivo
+        alt="Acme Inc Logo"
+        width={120}
+        height={120}
+        className="h-24 w-24"
+        priority
+      />
+      <span className="font-roboto font-black text-white text-[6vw] sm:text-[5vw] md:text-[3vw]">Singapur</span>
+    </Link>
+  </div>
 );
 
 export default Logo;
